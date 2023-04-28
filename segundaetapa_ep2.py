79b82d67ef0b645616e04e917e4806e16db075db
Edu0liveira <edumoliveira2005@gmail.com>
2023-04-28 11:24:22 -0300

    Codigo_corrigido

--- segundaetapa_ep2.py
+++ segundaetapa_ep2.py
@@ -2,27 +2,23 @@ def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
     # x é linha e y é coluna
     #frota é um dicionario
 
-    dc_final = {}
     lista_final = [] # vai pro dc final
-    sublista = [] # vai pra lista_final
-
-    for j in frota.keys():
-        if j == nome_navio:         #tenta rodar sem o trecho da linha 9 até 12 pra entender antes
-            valor = frota[j]
-            lista_final.append
+    lista = [] # vai pra lista_final
 
     for i in range(0,tamanho):
         if orientacao == 'vertical':
             lista_i = [linha+i, coluna]
-            sublista.append(lista_i)
+            lista.append(lista_i)
 
         if orientacao == 'horizontal':
             lista_i = [linha, coluna+i]
-            sublista.append(lista_i)
+            lista.append(lista_i)
 
 
+    if nome_navio in frota:
+        frota[nome_navio].append(lista)
+    else:
+        lista_final.append(lista)
+        frota[nome_navio] = lista_final
 
-    lista_final.append(sublista)
-    dc_final[nome_navio] = lista_final
-
-    return dc_final
+    return frota
