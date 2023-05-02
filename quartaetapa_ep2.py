43cbf8c407084fc8f241ba7bafebe70c233f2a2a
Edu0liveira <edumoliveira2005@gmail.com>
2023-05-01 22:53:19 -0300

    aaaaa

--- quartaetapa_ep2.py
+++ quartaetapa_ep2.py
@@ -2,18 +2,20 @@ def posiciona_frota(dicionario_de_infos_dos_navios):
     listas = 10*[0]
     tabuleiro = 10*listas
 
-    for lista_posicoes in dicionario_de_infos_dos_navios.values():
+    for lista in dicionario_de_infos_dos_navios.values():
+        for posicoes in lista:
 
-        for posicao in lista_posicoes:
-            linha = posicao[0]
-            coluna = posicao[1]
+            for posicao in posicoes:
+                linha = posicao[0]
+                coluna = posicao[1]
 
-            tabuleiro[linha][coluna] = 1
+                tabuleiro[linha][coluna] = 1
 
 
     return tabuleiro
 
 #nao to entendendo o erro, ve ai 
+#int object does not support item assignment
 
 
 
